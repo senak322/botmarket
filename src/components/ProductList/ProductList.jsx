e55f926a718980b3c575d62da1f8e9496909d3f8
Aleksandr Viznyi <senak98@yandex.ru>
2023-10-01 20:29:22 +0400

    fix

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./ProductList.css"
+import { Link } from 'react-router-dom';
 
 const ProductList = () => {
     return (
         <div>
             ProductList
+            <Link to="/form">Заполнить форму</Link>
         </div>
     );
 };
