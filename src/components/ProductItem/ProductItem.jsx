a49979bd069db8a2592febdca9469eeb83e7cee3
Aleksandr Viznyi <senak98@yandex.ru>
2023-10-11 01:14:17 +0700

    add item

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,22 @@
+import Button from "../Button/Button";
+import "./ProductItem.css";
+
+const ProductItem = () => {
+  return (
+    <div className={"product " + className}>
+      <div className="img" />
+      <div className="title">{product.title}</div>
+      <div className="description">{product.description}</div>
+      <div className="price">
+        <span>
+          Стоимость: <b>{product.price}</b>
+        </span>
+      </div>
+      <Button className={'add-btn'} onClick={onAddHandler}>
+        Добавить в корзину
+      </Button>
+    </div>
+  );
+};
+
+export default ProductItem;
