2cc82e7b86d601e9e965ff5e9af36cbc4231f5b5
Aleksandr Viznyi <senak98@yandex.ru>
2023-10-13 03:55:07 +0700

    fix fetch

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -72,7 +72,7 @@ const ProductList = () => {
         totalPrice: getTotalPrice(addedItems),
         queryId: queryId
     };
-    fetch('http://45.131.96.109:8000/web-data', {
+    fetch('http://192.168.0.2:8000/web-data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
