e7e607699f2a939ba20e1afbe3990f59e441bb2b
Aleksandr Viznyi <senak98@yandex.ru>
2023-10-12 20:59:58 +0700

    add total price

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import "./ProductList.css"
 import ProductItem from '../ProductItem/ProductItem';
+import {useTelegram} from "../../hooks/useTelegram"
 
 const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
@@ -13,11 +14,38 @@ const products = [
     {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
+
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([])
 
+    const {tg} = useTelegram()
+
     const onAdd = (product) => {
-        
+        const alreadyAdded = addedItems.find(item => item.id === product.id)
+
+        let newItems = []
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id)
+        } else {
+            newItems = [...addedItems, product]
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
     }
 
     return (
